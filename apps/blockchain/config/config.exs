26e2cf45c259ea6b05ca525d866b30f50c0e83b0
Lokraan <Artinaire@gmail.com>
2018-08-16 16:20:42 -0500

    Difficulty First.

--- apps/blockchain/config/config.exs
+++ apps/blockchain/config/config.exs
@@ -2,8 +2,8 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-	config :blockchain,
-		target_bits: 26 # diffulty
+  config :blockchain,
+    target_bits: 26 # diffulty
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
---
+++ apps/blockchain/lib/blockchain/difficulty.ex
@@ -0,0 +1,9 @@
+
+defmodule Blockchain.Difficulty do
+
+	@spec get_next_difficulty(Blockchain) :: integer
+	def get_next_difficulty(chain) do
+		target_bits
+	end
+
+end
